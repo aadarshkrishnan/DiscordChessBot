fc827031784a78c54412e7494ddedf3bcbfbb7e2
aadarshkrishnan <akrish49@illinois.edu>
2021-06-01 18:13:52 -0500

    got engine to run without discord

--- chessbot.py
+++ chessbot.py
@@ -1,17 +1,27 @@
-from stockfish import Stockfish
-from discord.ext import commands
+import chess
+import chess.engine
+
+engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+board = chess.Board()
 
 
-stockfish = Stockfish("/Users/aadarshkrishnan/discord-chatbox/stockfish/stockfish-9-64")
+board = chess.Board("r1bqkbnr/p1pp1ppp/1pn5/4p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR")
+info = engine.analyse(board, chess.engine.Limit(depth=20))
+print(info["pv"])
 
-bot = commands.Bot(command_prefix='!')
+engine.quit()
 
-@bot.command(name = "calculate", help = "set up position to calculate using FEN notation")
-async def calculate(ctx, notation: str):
-    stockfish.set_fen_position(notation)
-    await ctx.send(stockfish.get_best_move)
 
-with open ("CHESSBOT_TOKEN.txt", "r") as token_file:
-    TOKEN = token_file.read
-    print("Token file read")
-    bot.run(TOKEN)
+#from discord.ext import commands
+
+#bot = commands.Bot(command_prefix='!')
+
+
+#@bot.command(name = "calculate", help = "set up position to calculate using FEN notation")
+#async def calculate(ctx, notation: str):
+    #print("hi")
+
+#with open ("CHESSBOT_TOKEN.txt", "r") as token_file:
+    #TOKEN = token_file.read
+    #print("Token file read")
+    #bot.run(TOKEN)
