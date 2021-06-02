2e04994328a9fb6a6e22623cd3c775ae8716ee0c
aadarshkrishnan <akrish49@illinois.edu>
2021-06-02 11:10:37 -0500

    fixed display format

--- chessbot.py
+++ chessbot.py
@@ -14,7 +14,8 @@ async def calculate(ctx, notation: str):
     """This function analyzes a given board and gives the best move"""
     board = chess.Board(notation)
     info = engine.analyse(board, chess.engine.Limit(depth=20))
-    await ctx.send(info["pv"])
+    information = str(info["pv"])
+    await ctx.send(information[16:20])
 
 with open("CHESSBOT_TOKEN.txt", "r") as token_file:
     TOKEN = token_file.read()
