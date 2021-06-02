a5912cd706dddbc9f4c9555d49c49ebedc2ea895
aadarshkrishnan <akrish49@illinois.edu>
2021-06-02 15:44:03 -0500

    Added black move calculation

--- chessbot.py
+++ chessbot.py
@@ -2,20 +2,27 @@
 
 import chess
 import chess.engine
+import chess.svg
 
 from discord.ext import commands
 
-engine = chess.engine.SimpleEngine.popen_uci("stockfish")
-
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name="calculate")
-async def calculate(ctx, notation: str):
-    """This function analyzes a given board and gives the best move"""
-    board = chess.Board(notation)
+@bot.command(name="calculate_fen")
+async def calculate_fen(ctx, *fen_notation):
+    """This function analyzes a given board given in fen notation and gives the best move"""
+    fen_notation = ' '.join(fen_notation)
+    engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+    board = chess.Board(fen_notation)
     info = engine.analyse(board, chess.engine.Limit(depth=20))
     information = str(info["pv"])
     await ctx.send(information[16:20])
+    
+
+#@bot.command(name="calculate_pgn")
+#async def calculate_pgn(ctx, pgn_notation:str):
+#    """This function analyzes a given board given in pgn notation and gives the best move"""
+#    await ctx.send(pgn_notation)
 
 with open("CHESSBOT_TOKEN.txt", "r") as token_file:
     TOKEN = token_file.read()
