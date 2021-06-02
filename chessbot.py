1b4827ca1f6f32e7ed5d225fdb1f0e7579f7e24f
aadarshkrishnan <akrish49@illinois.edu>
2021-06-02 10:54:00 -0500

    fixed style with pylint

--- chessbot.py
+++ chessbot.py
@@ -1,15 +1,17 @@
+"""This module contains some functions to use a discord chessbot"""
+
 import chess
 import chess.engine
 
 from discord.ext import commands
 
 engine = chess.engine.SimpleEngine.popen_uci("stockfish")
-board = chess.Board()
 
-bot = commands.Bot(command_prefix= '!')
+bot = commands.Bot(command_prefix='!')
 
-@bot.command(name = "calculate")
+@bot.command(name="calculate")
 async def calculate(ctx, notation: str):
+    """This function analyzes a given board and gives the best move"""
     board = chess.Board(notation)
     info = engine.analyse(board, chess.engine.Limit(depth=20))
     await ctx.send(info["pv"])
